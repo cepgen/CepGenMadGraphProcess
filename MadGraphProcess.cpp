f19df3163a847d1ce53d45b267822f7add7a3d1a
Laurent Forthomme <laurent.forthomme@cern.ch>
2023-06-05 22:27:38 +0200

    Code-format

--- MadGraphProcess.cpp
+++ MadGraphProcess.cpp
@@ -1,88 +1,75 @@
 //=============================================================================
 // NOLI SE TANGERE
-#include "CepGen/Processes/KTProcess.h"
-#include "CepGen/Modules/ProcessesFactory.h"
-#include "CepGen/Event/Event.h"
-#include "CepGen/Core/Exception.h"
 #include "CPPProcess.h"
+#include "CepGen/Core/Exception.h"
+#include "CepGen/Event/Event.h"
+#include "CepGen/Modules/ProcessesFactory.h"
+#include "CepGen/Process/KTProcess.h"
 
 using namespace cepgen;
 
-class MadGraphProcess : public proc::KTProcess
-{
-  public:
-    MadGraphProcess( const ParametersList& params );
-    proc::ProcessPtr clone( const ParametersList& ) const override {
-      return proc::ProcessPtr( new MadGraphProcess( *this ) );
-    }
+class MadGraphProcess : public proc::KTProcess {
+public:
+  MadGraphProcess(const ParametersList& params);
+  proc::ProcessPtr clone(const ParametersList&) const override { return proc::ProcessPtr(new MadGraphProcess(*this)); }
 
-  private:
-    void preparePhaseSpace() override;
-    double computeKTFactorisedMatrixElement() override;
-    void fillCentralParticlesKinematics() override;
+private:
+  void preparePhaseSpace() override;
+  double computeKTFactorisedMatrixElement() override;
+  void fillCentralParticlesKinematics() override;
 
-    const std::string param_card_;
-    CPPProcess proc_;
-    Momentum qt_1_, qt_2_;
-    std::vector<Momentum> p_cent_;
-    std::vector<double*> momenta_;
+  const std::string param_card_;
+  CPPProcess proc_;
+  Momentum qt_1_, qt_2_;
+  std::vector<Momentum> p_cent_;
+  std::vector<double*> momenta_;
 };
 
-MadGraphProcess::MadGraphProcess( const ParametersList& params ) :
-  KTProcess( params, std::array<pdgid_t,2>{}, std::vector<pdgid_t>{} ),
-  param_card_( params.get<std::string>( "parametersCard", "param_card.dat" ) )
-{
+MadGraphProcess::MadGraphProcess(const ParametersList& params)
+    : KTProcess(params, std::array<pdgid_t, 2>{}, std::vector<pdgid_t>{}),
+      param_card_(params.get<std::string>("parametersCard", "param_card.dat")) {
   description_ = proc_.name();
-  params_.set<std::string>( "description", description_ );
-  CG_INFO( "MadGraphProcess" )
-    << "Process considered: " << description_;
+  params_.set<std::string>("description", description_);
+  CG_INFO("MadGraphProcess") << "Process considered: " << description_;
 }
 
-void
-MadGraphProcess::preparePhaseSpace()
-{
+void MadGraphProcess::preparePhaseSpace() {
   //--- initialise the process
   try {
-    proc_.initProc( param_card_ );
-  } catch ( const char* chr ) {
-    throw CG_FATAL( "MadGraphProcess" )
-      << "Failed to initialise parameters card at \"" << param_card_ << "\":\n\t"
-      << chr;
+    proc_.initProc(param_card_);
+  } catch (const char* chr) {
+    throw CG_FATAL("MadGraphProcess") << "Failed to initialise parameters card at \"" << param_card_ << "\":\n\t"
+                                      << chr;
   }
-  if ( proc_.nprocesses > 1 )
-    throw CG_FATAL( "MadGraphProcess" )
-      << "Multi-processes matrix elements are not supported!";
-  momenta_.resize( proc_.nexternal );
-  for ( size_t i = proc_.ninitial; i < proc_.nexternal; ++i )
-    p_cent_.emplace_back( 0., 0., 0., proc_.getMasses().at( i ) );
+  if (proc_.nprocesses > 1)
+    throw CG_FATAL("MadGraphProcess") << "Multi-processes matrix elements are not supported!";
+  momenta_.resize(proc_.nexternal);
+  for (size_t i = proc_.ninitial; i < proc_.nexternal; ++i)
+    p_cent_.emplace_back(0., 0., 0., proc_.getMasses().at(i));
 }
 
-double
-MadGraphProcess::computeKTFactorisedMatrixElement()
-{
-  qt_1_ = Momentum::fromPtEtaPhi( qt1_, 0., phi_qt1_ );
-  qt_2_ = Momentum::fromPtEtaPhi( qt2_, 0., phi_qt2_ );
-  qt_1_.setMass( 0. );
-  qt_2_.setMass( 0. );
+double MadGraphProcess::computeKTFactorisedMatrixElement() {
+  qt_1_ = Momentum::fromPtEtaPhi(qt1_, 0., phi_qt1_);
+  qt_2_ = Momentum::fromPtEtaPhi(qt2_, 0., phi_qt2_);
+  qt_1_.setMass(0.);
+  qt_2_.setMass(0.);
   momenta_[0] = qt_1_.pVector().data();
   momenta_[1] = qt_2_.pVector().data();
-  for ( size_t i = 0; i < p_cent_.size(); ++i )
-    momenta_[2+i] = p_cent_.at( i ).pVector().data();
-  proc_.setMomenta( momenta_ );
+  for (size_t i = 0; i < p_cent_.size(); ++i)
+    momenta_[2 + i] = p_cent_.at(i).pVector().data();
+  proc_.setMomenta(momenta_);
   proc_.sigmaKin();
   const double* me = proc_.getMatrixElements();
-  return me[0]*constants::GEVM2_TO_PB;
+  return me[0] * constants::GEVM2_TO_PB;
 }
 
-void
-MadGraphProcess::fillCentralParticlesKinematics()
-{
+void MadGraphProcess::fillCentralParticlesKinematics() {
   const auto& mom_filled = proc_.getMomenta();
-  for ( const auto& mom4 : mom_filled ) {
-    Momentum mom( mom4 );
+  for (const auto& mom4 : mom_filled) {
+    Momentum mom(mom4);
     //CG_INFO( "" ) << mom;
   }
 }
 
-REGISTER_PROCESS( "mg5", "", MadGraphProcess )
+REGISTER_PROCESS("mg5", "", MadGraphProcess)
 //=============================================================================
--- Makefile
+++ Makefile
@@ -1,9 +1,5 @@
-ifndef MG5DIR
-override MG5DIR=mg5
-endif
-ifndef LIB
-override LIB=libCepGenUserProcess.so
-endif
+MG5DIR := $(or $(MG5DIR), mg5)
+LIB := $(or $(LIB), libCepGenUserProcess.so)
 
 PSRC=$(wildcard $(MG5DIR)/SubProcesses/P1_*/*.cc $(MG5DIR)/src/*.cc)
 POBJ=$(PSRC:.cc=.o)
@@ -14,7 +10,7 @@ CEPGEN_PATH := $(or $(CEPGEN_PATH), /usr)
 CEPGEN_LIBS_DIR := $(or $(CEPGEN_LIBS_DIR), $(CEPGEN_PATH)/lib64)
 CEPGEN_INCLUDE_DIR := $(or $(CEPGEN_INCLUDE_DIR), $(CEPGEN_PATH)/include)
 
-CFLAGS=-I$(CEPGEN_INCLUDE_DIR) $(PINC) -L$(CEPGEN_LIBS_DIR) -lCepGenCore -fPIC -std=c++14 -Wall -pedantic -g
+CFLAGS=-I$(CEPGEN_INCLUDE_DIR) $(PINC) -L$(CEPGEN_LIBS_DIR) -lCepGen -fPIC -std=c++14 -Wall -pedantic -g
 
 .PHONY: lib clean
 
